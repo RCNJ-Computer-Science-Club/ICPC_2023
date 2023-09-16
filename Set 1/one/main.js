988c33be85cad2bdd2761d3df2934f6f08ad690b
Daniel Rajakumar <danielelvisr@gmail.com>
2023-09-16 12:40:19 -0400

    set 1

--- Set 1/one/main.js
+++ Set 1/one/main.js
@@ -1,19 +1,19 @@
 
-function main() {
-  set = [
-    [20, 2],
-    [30, 6],
-    [10, 7],
+let input = [
+    [ 3, [20, 2], [30, 6], [10, 7] ],
+    [ 2, [60, 1], [30, 5] ],
+    [ 4, [15, 1], [25, 2], [30, 3], [10, 5] ]
   ]
 
-  console.log(calculate(set))
+function main() {
+  input.forEach(i => calculate(i[0], i.slice(1)))
 }
 
 
-function calculate(sets){
+function calculate(n, sets) {
   let sum = 0, num = 0;
 
-  for (let i = 0; i < sets.length; i++) {
+  for (let i = 0; i < n; i++) {
       num = sets[i][0]
 
       if (i < 1) num *= sets[i][1]
@@ -22,17 +22,18 @@ function calculate(sets){
       sum += num;
   }
 
-  return sum;
+  console.log(sum, "miles")
 }
 
 
 
-
-
-
-
-
-
 main()
+
+
+
+
+
+
+
 
 
