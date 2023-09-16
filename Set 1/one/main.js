144d45a44d7609d1598e2febcd33b85d4cfbb114
Daniel Rajakumar <danielelvisr@gmail.com>
2023-09-16 11:46:09 -0400

    push

--- Set 1/one/main.js
+++ Set 1/one/main.js
@@ -1,2 +1,38 @@
 
+function main() {
+  set = [
+    [20, 2],
+    [30, 6],
+    [10, 7],
+  ]
+
+  console.log(calculate(set))
+}
+
+
+function calculate(sets){
+  let sum = 0, num = 0;
+
+  for (let i = 0; i < sets.length; i++) {
+      num = sets[i][0]
+
+      if (i < 1) num *= sets[i][1]
+      else       num *= sets[i][1] - sets[i - 1][1]
+
+      sum += num;
+  }
+
+  return sum;
+}
+
+
+
+
+
+
+
+
+
+main()
+
 
